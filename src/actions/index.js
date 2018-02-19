acf9e116d27eaba8028affc165afec2a84bc35eb
Simon Rousseau <simonrousseau22@gmail.com>
2018-02-19 18:35:29 +0100

    Fix some mistakes in ActionType File

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import * as types from '../constants/ActionTypes'
 
 let nextMessageId = 0
-const nextUserId = 0
+var nextUserId = 0
 
 export const addMessage = (message, author) => ({
 	type: types.ADD_MESSAGE,
@@ -23,8 +23,8 @@ export const messageReceived = (message, author) => ({
 	author
 })
 
-export cosnt populateUseresList = users => ({
-	type: types.USER_LIST,
+export const populateUseresList = users => ({
+	type: types.USERS_LIST,
 	users
 })
 
--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,4 +1,4 @@
 export const ADD_MESSAGE = "ADD_MESSAGE"
 export const MESSAGE_RECEIVED = "MESSAGE_RECEIVED"
 export const ADD_USER = "ADD_USER"
-export const USER_LIST = "USERS_LIST"
+export const USERS_LIST = "USERS_LIST"
