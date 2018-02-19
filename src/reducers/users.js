ae2b22ae7cfdba52a123385c7651db03127d5268
Simon Rousseau <simonrousseau22@gmail.com>
2018-02-19 20:51:05 +0100

    Fix User Reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -4,11 +4,11 @@ const users = (state = [], action) => {
 	switch (action.type) {
 		case types.ADD_USER:
 			return state.concat([{
-				name:action.name,
+				name: action.name,
 				id: action.id
 			}])
 		case types.USERS_LIST:
-			return action.user
+			return action.users
 		default:
 			return state
 	}
