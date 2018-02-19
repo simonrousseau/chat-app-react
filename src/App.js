a92ec82ca5d1b89143bb61f2e6d71193e94af6c9
Simon Rousseau <simonrousseau22@gmail.com>
2018-02-19 18:35:50 +0100

    Fix App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react"
 import "./App.css"
-
 import { Sidebar } from "./containers/Sidebar"
 import { MessagesList } from "./containers/MessagesList"
 import { AddMessage } from "./containers/AddMessage"
