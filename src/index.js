b5cd78acccb8c8ed1865b3e07cecd61fb3301073
Simon Rousseau <simonrousseau22@gmail.com>
2018-02-19 20:17:37 +0100

    Update index.js (src)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore , applyMiddleware} from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import setupSocket from './sockets'
+import reducers from './reducers'
+import handleNewMessage from './sagas'
+import username from './utils/name'
 
-import chat from './reducers'
+const sagaMiddleware = createSagaMiddleware()
 
 //Stockage des données
-const store = createStore(chat)
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware)
+)
+
+const socket = setupSocket(store.dispatch, username)
+
+sagaMiddleware.run(handleNewMessage, {socket, username})
 
 ReactDOM.render(
   <Provider store={store}>
